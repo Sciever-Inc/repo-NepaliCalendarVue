2b064ffb5866a59ddecff04f3e347c6cd97190d9
Dell <tekrajpant224@gmail.com>
2021-01-17 18:55:54 +0545

    vue build created for the component

---
+++ build/rollup.config.js
@@ -0,0 +1,17 @@
+import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+export default {
+    input: 'src/index.js', // Path relative to package.json
+    output: {
+        name: 'Calender',
+        exports: 'named',
+    },
+    plugins: [
+        commonjs(),
+        vue({
+            css: true, // Dynamically inject css as a <style> tag
+        }),
+        buble(), // Transpile to ES5
+    ],
+};
